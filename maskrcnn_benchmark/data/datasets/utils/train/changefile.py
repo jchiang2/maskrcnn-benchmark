a17c5510a158c533e697e2516016878c43b60dab
jchiang2 <jchiang2.andrew.cmu.edu>
2019-04-09 16:03:43 -0400

    Updated

--- maskrcnn_benchmark/data/datasets/utils/train/changefile.py
+++ maskrcnn_benchmark/data/datasets/utils/train/changefile.py
@@ -3,6 +3,7 @@ import os
 files = os.listdir("train/Bboxes_mydataset")
 
 for file in files:
+	assert len(file) < 20 
 	path = os.path.join("train/Bboxes_mydataset", file)
 	name = file[7:-4]
 	index = name.zfill(8)
@@ -10,4 +11,4 @@ for file in files:
 	movie_index = movie.zfill(2)
 	new_name = movie_index + "_Frame_" + index + file[-4:]
 	new_path = os.path.join("train/Bboxes_mydataset", new_name)
-	os.rename(path, new_path)
+	# os.rename(path, new_path)
